848429b4f23306fb9f0315ac3e8446a33347da4c
Carlos <poncesantanacarlos@gmail.com>
2026-09-25 18:27:10 +0100

    corrections added

--- backend/src/main/java/com/jobFlow/jobFlow/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/jobFlow/jobFlow/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.jobFlow.jobFlow.exception;
 
-import java.lang.reflect.Field;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -9,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice 
